d40740e2aae54c3521d68d2ba9b91f50ce522ec2
Daniel <65277548+Daniel4-Scratch@users.noreply.github.com>
2020-07-26 12:49:21 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#Enter project name
+Project_Name = "engine"
+#Enter proje3ct author
+Project_Author = "jens"
+#Run the code and it will print the link
+print("SnapCups \nCopy and paste the link")
+print("Link:")
+print("\nhttps://snap.berkeley.edu/embed?project=" + Project_Name + "&user=" +
+      Project_Author +
+      "&showTitle=true&showAuthor=true&editButton=true&pauseButton=true")
+print("\nSnapCups by Danielthebanana4")
