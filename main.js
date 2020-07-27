513614b3290f416c5f6e07ebe9ca1b2d93ed2126
R4356th <61620631+R4356th@users.noreply.github.com>
2020-07-27 15:54:58 +0600

    Create JavaScript implementation

---
+++ main.js
@@ -0,0 +1,7 @@
+//Copyright 2020 by R4356th (github.com/R4356th)
+const ProjectName = 'Engine';
+const ProjectAuthor = 'jens';
+document.write("https://snap.berkeley.edu/embed?project=" + ProjectName +
+"&user=" + ProjectAuthor +
+"&showTitle=true&showAuthor=true&editButton=true&pauseButton=true"
+}
